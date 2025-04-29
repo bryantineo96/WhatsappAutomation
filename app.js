43899639b4f1c2d171e5970d07287ac546e7f4fb
bryantineo96 <66627678+bryantineo96@users.noreply.github.com>
2025-04-29 15:45:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -52,9 +52,7 @@ const wwebVersion = '2.2407.3';
 
 const client = new Client({
   authStrategy: new LocalAuth(), // your authstrategy here
-  puppeteer: {
-    headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
+
   webVersionCache: {
     type: 'remote',
     remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
